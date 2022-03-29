f991cc31a293c08e5cd1cdba93b4dc66c7170fc2
William-Nimitz <mingchaitest27@gmail.com>
2022-03-29 18:44:28 +0800

    lendmesh update v1

--- assets/js/angJs/bankDetails.js
+++ assets/js/angJs/bankDetails.js
@@ -7,6 +7,9 @@ app.controller('BankDetailCtrl', function (customFunc) {
     this.bankShortName = window.location.search.replace(/\?/, '').split('&')[0].split('=')[1];
     this.bank = '';
     this.bankContent = {};
+
+    this.activeTab = 0;
+    this.tabContentHtml = '';
     //get all banks
     customFunc.httpRequest(`${_base}${_banks}`, "GET")
         .then(res => {
@@ -17,8 +20,54 @@ app.controller('BankDetailCtrl', function (customFunc) {
             customFunc.httpRequest(`${_base}${thisObj.bank.file}`, "GET")
                 .then(bank => {
                     thisObj.bankContent = bank.data;
-                    console.log(thisObj.bankContent.content);
                 })
+                .then(() => {thisObj.tabClick(0)})
         
         })
+    // functions 
+    this.tabClick = function(index) {
+        this.activeTab = index;
+        const tabContent = this.bankContent.content[index];
+
+        let tabHtmlContent = `<ul class="${this.ulFormat(tabContent.format)}">`;
+
+        tabContent.content.forEach(con => {
+            tabHtmlContent += `<li>${this.subLi(con)}</li>`;
+        })
+
+        tabHtmlContent += '</ul>';
+        this.tabContentHtml = tabHtmlContent;
+    }
+    this.subLi = function (con) {
+        return angular.isString(con) ?
+            `<p>${con}</p>`
+            :
+            `<p class="font-weight-bold">${con.header}</p>
+                <ul class="${this.ulFormat(con.format)}">
+                    ${con.content.reduce((prev, curr, index) => (index === 1) ? `<li>${this.subLi(prev)}</li><li>${this.subLi(curr)}</li>` : `${prev}<li>${this.subLi(curr)}</li>`)}
+                </ul>`;
+    }
+    this.ulFormat = function(format) {
+        let listStyleType = 'noneUl';
+
+        switch (format) {
+            case "none":
+                listStyleType = 'noneUl';
+                break;
+            case "disc":
+                listStyleType = 'discUl';
+                break;
+            case "circle":
+                listStyleType = 'circleUl';
+                break;
+            case "1":
+                listStyleType = 'decimalUl';
+                break;
+            default:
+                listStyleType = 'noneUl';
+                break;
+        }
+        return listStyleType;
+    }
+    
 }); 
