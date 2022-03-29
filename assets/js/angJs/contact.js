2f3d419c0a9009a9401074e0d375ea8c7c751e97
William-Nimitz <mingchaitest27@gmail.com>
2022-03-30 00:25:14 +0800

    lendmesh v5

--- assets/js/angJs/contact.js
+++ assets/js/angJs/contact.js
@@ -1,15 +1,14 @@
 app.controller('contactCtrl', function(customFunc) {
     
     this.contact = {
-        customerName: "",
-        customerEmail: "",
-        customerPhone: "",
-        customerMessage: "",
+        name: "",
+        emailid: "",
+        phone: "",
+        message: "",
     }
     this.contactUsSendEmail = function() {
-        
+
         const contactUrl = customFunc.serverUrl + "contact-us-send-email";
-        console.log(this.contact, contactUrl);
         customFunc.httpRequest(contactUrl, 'post', this.contact)
                   .then(function(res) {
                      console.log(res);
